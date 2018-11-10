ac010a32ecbf4504fa8c8de964018323bae932f1
kapoor-rakshit <kapoor_rakshit@yahoo.co.in>
2018-11-10 16:26:23 +0530

    added charts, prod build, yet to deployment

--- MEAN/source_angular/bookMyBusApp/src/app/adminComponent/topCities.ts
+++ MEAN/source_angular/bookMyBusApp/src/app/adminComponent/topCities.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { CityService } from './city_service';
 import { City } from './cityinterface';
 import { ActivatedRoute, Params } from '@angular/router';
+import {Chart} from 'chart.js';
 
 import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
 
@@ -13,23 +14,77 @@ import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
 export class TopCities implements OnInit{
 
     cities: City[];
+    chart: any;             // hold chart
+    namesCity: any = [];
+    counterCity: any = [];
+    ctx: any;
 
-    constructor(private _cityService: CityService, private route: ActivatedRoute, private spinnerServive: Ng4LoadingSpinnerService){
+    constructor(private _cityService: CityService, private elementRef: ElementRef, private route: ActivatedRoute, private spinnerServive: Ng4LoadingSpinnerService){
         this.spinnerServive.show();
-        this.getTopCities();
-        this.spinnerServive.hide();
-    }
-
-    getTopCities(){
         this._cityService.getTopCities().subscribe(
             (data: any) => {
                 this.cities = data;
+                this.cities.forEach(c => {
+                    this.namesCity.push(c.name);
+                    this.counterCity.push(c.counter);
+                });
+                this.spinnerServive.hide();
             },
             err => console.log(err)
         );
     }
 
     ngOnInit(){
+
+        this.ctx = this.elementRef.nativeElement.querySelector("canvas");
+        // create the chart using the chart canvas
+        this.chart = new Chart(this.ctx, {
+        type: 'bar',                                           
+        data: {
+            labels : this.namesCity,                         // City Names
+            datasets : [{
+                label: "Bookings",                // LEGEND
+                backgroundColor: [ "#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA","#ABCDEF", "#AADDDD", "#ABCABC" , "#FAFAAA"],
+                borderColor: [ "#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA","#ABCDEF", "#AADDAA", "#ABCABC" , "#FAFAAA"],
+                data : this.counterCity,                     // Booking Count
+                spanGaps: false
+            }]
+          },
+        options: {
+            animation: {
+                easing :"easeOutBounce"
+            },
+            scales: {
+                xAxes: [{
+                    scaleLabel: {
+                        display: true,
+                        labelString: "Cities"
+                    }
+                }],
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    },
+                    scaleLabel: {
+                        display: true,
+                        labelString: "Bookings"
+                    }
+                }]
+            },
+            layout:{
+                padding:{                                    // padding to the sides of the chart canvas
+                left: 100,
+                right:0,
+                top:0,
+                bottom:100
+                }
+            },
+            title:{
+                display:true,
+                text:"Top Destinations"
+            }
+        }
+        });
     }
 
 }
